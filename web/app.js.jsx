ec313ce25bdafa2178c10b30dc697214ce6b5171
Joao Diogo Costa <jdscosta91@gmail.com>
2016-04-29 03:52:31 +0000

    Split components and pass data around through props

--- web/app.js.jsx
+++ web/app.js.jsx
@@ -1,5 +1,58 @@
-// Define the React Component
+var Bar = React.createClass({
+  propTypes: {
+    bar: React.PropTypes.object
+  },
+
+  render: function() {
+    var bar = this.props.bar;
+
+    return (
+      <div className='bar'>
+        <span className='name'>{bar.name}</span>
+        <span className='votes'>{bar.votes}</span>
+      </div>
+    );
+  }
+});
+
+
 var BarList = React.createClass({
+  propTypes: {
+    bars: React.PropTypes.array
+  },
+
+  renderBars: function() {
+    return this.props.bars.map(function(bar) {
+      return (
+        <Bar bar={bar} key={bar.name} />
+      );
+    }.bind(this));
+  },
+
+  render: function() {
+    return (
+      <div>{this.renderBars()}</div>
+    );
+  }
+});
+
+
+var OrderBox = React.createClass({
+  propTypes: {
+    orderByVotes: React.PropTypes.func
+  },
+
+  render: function() {
+    return (
+      <div className="order-bars-wrapper">
+        Order by: <button id="order-by-votes-button" onClick={this.props.orderByVotes}>Votes</button>
+      </div>
+    );
+  }
+})
+
+
+var BarApp = React.createClass({
 
   getInitialState: function() {
     var bars = [
@@ -34,39 +87,20 @@ var BarList = React.createClass({
     this.setState({ bars: orderedBars });
   },
 
-  renderOrderBox: function() {
-    return (
-      <div className="order-bars-wrapper">
-        Order by: <button id="order-by-votes-button" onClick={this.orderByVotes}>Votes</button>
-      </div>
-    );
-  },
-
-   renderBars: function() {
-    var bars = this.state.bars;
-
-    return bars.map(function(bar) {
-      return (
-        <div className='bar' key={bar.name}>
-          <span className='name'>{bar.name}</span>
-          <span className='votes'>{bar.votes}</span>
-        </div>
-      );
-    }.bind(this));
-  },
-
   render: function() {
     return (
       <div>
-        {this.renderOrderBox()}
-        {this.renderBars()}
+        <OrderBox orderByVotes={this.orderByVotes} />
+        <BarList bars={this.state.bars} />
       </div>
     );
   }
 });
 
+
 // Get HTML container where we will mount the React Component
 var barListEl = document.getElementById('bar-list');
 
+
 // Render the React Component
-ReactDOM.render(<BarList />, barListEl);
+ReactDOM.render(<BarApp />, barListEl);
