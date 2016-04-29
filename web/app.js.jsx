9975e009c01d42936655cd9850a22e9330f0e8ad
Joao Diogo Costa <jdscosta91@gmail.com>
2016-04-29 02:59:31 +0000

    Render bars from state

--- web/app.js.jsx
+++ web/app.js.jsx
@@ -1,20 +1,46 @@
 // Define the React Component
 var BarList = React.createClass({
+
+  getInitialState: function() {
+    var bars = [
+      {
+        name: 'Moelas',
+        votes: 100
+      },
+      {
+        name: 'NB',
+        votes: 50
+      },
+      {
+        name: 'Bigorna',
+        votes: 110
+      },
+      {
+        name: 'Whats Up Doc',
+        votes: 12
+      }
+    ];
+
+    return { bars: bars };
+  },
+
+  renderBars: function() {
+    var bars = this.state.bars;
+
+    return bars.map(function(bar) {
+      return (
+        <div className='bar' key={bar.name}>
+          <span className='name'>{bar.name}</span>
+          <span className='votes'>{bar.votes}</span>
+        </div>
+      );
+    }.bind(this));
+  },
+
   render: function() {
     return (
       <div>
-        <div className='bar'>
-          <span className='name'>NB</span>
-          <span className='votes'>10</span>
-        </div>
-        <div className='bar'>
-          <span className='name'>Moelas</span>
-          <span className='votes'>100</span>
-        </div>
-        <div className='bar'>
-          <span className='name'>Piano</span>
-          <span className='votes'>55</span>
-        </div>
+        {this.renderBars()}
       </div>
     );
   }
